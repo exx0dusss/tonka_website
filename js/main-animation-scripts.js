c0f3af0536475b07793b96180e143ed8c4e6f4ea
exx0dusss <82311111+exx0dusss@users.noreply.github.com>
2023-08-04 17:22:10 +0200

    ass
    ss

---
+++ js/main-animation-scripts.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const container = document.querySelector('.icon-container');
+    container.style.width = '100%'; // New width after loading
+});
