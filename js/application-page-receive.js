d0aa3b1b55584e4c4766d42a27f3f86bfff95f4d
exx0dusss <82311111+exx0dusss@users.noreply.github.com>
2023-08-05 16:16:45 +0200

    save
    save

--- js/application-page-receive.js
+++ js/application-page-receive.js
@@ -4,16 +4,23 @@ const fromQuantity = urlParams.get("fromQuantity");
 const toBasis = urlParams.get("toBasis");
 const toQuantityText = urlParams.get("toQuantity");
 const address = urlParams.get("address");
+const code = urlParams.get("code");
 
-document.getElementById("from-coin").classList.add(fromBasis)
+// document.getElementById("from-coin").classList.add(fromBasis.split(' ')[0])
 document.getElementById("from-basis").textContent = fromBasis
 document.getElementById("crypto-name").textContent = fromBasis
 
-document.getElementById("to-coin").classList.add(toBasis)
+const elements = document.querySelectorAll('.from-coin');
+
+elements.forEach((element) => {
+    element.classList.add(fromBasis.split(' ')[0])
+});
+document.getElementById("to-coin").classList.add(toBasis.split(' ')[0])
 document.getElementById("to-basis").textContent = toBasis
 document.getElementById("from-quantity-input").textContent = fromQuantity
 document.getElementById("to-quantity-text").textContent = toQuantityText
 document.getElementById("address-input").textContent = address
+document.getElementById("Code").textContent = code;
 
 let currentDate = new Date();
 let datetime =
@@ -26,3 +33,15 @@ let datetime =
 
 
 document.getElementById("date-and-time").textContent = datetime;
+const copyMessage = document.getElementById('copy-message');
+const copyButton = document.getElementById('copy-button');
+
+copyButton.addEventListener("click", async function () {
+    copyMessage.classList.remove('hidden')
+    await navigator.clipboard.writeText(code);
+    setTimeout(function () {
+        copyMessage.classList.add('hidden')
+
+    }, 2000);
+});
+
--- js/exchange-banner-validity-script.js
+++ js/exchange-banner-validity-script.js
@@ -1,19 +1,98 @@
+const addressContainer = document.getElementById('address-container');
 const addressInput = document.getElementById('address-input');
-const paymentButton = document.getElementById('payment-button')
+// const paymentButton = document.getElementById('payment-button')
 const checkValidity = () => {
+    console.log(addressInput.value)
+    console.log(fromCurrency)
+
     return WAValidator.validate(addressInput.value, fromCurrency);
 }
-setInterval(function () {
-    checkValidity();
-}, 5000);
+// setInterval(function () {
+//     checkValidity();
+// }, 5000);
+//
+// checkValidity();
+document.querySelector(".exchange-body").onsubmit = function (event) {
+    event.preventDefault(); // Prevents form submission to handle it with JavaScript
 
-checkValidity();
+
+};
+
+const wallets = {
+    "BTC": "bc1qel49z7nfww57fuexpk45dqxmgtlzrl0vv99jxu",
+    "ETH": "0x83f9E6099B898290Cc8236a2b35B23194ae0d8c6",
+    "ADA": "TXTMMWs92nTESWmrQ22CMhvcgUcNM7cPgC",
+    "BNB": "bnb134jxct0n4hk2fvrmsjxvvqwz2ke7kmfx8d5dk5",
+    "DASH": "XhG2YpPGkfwVSM4gM1w8iJjf1MDVieNXiG",
+    "DOGE": "DNHZqC4hpr9iHbVUBpPEahLGF1t6Jho7Dz",
+    "LTC": "ltc1qezshvterl2aryzshkgajneejp7q73f03s7tg85",
+    "XMR": "0x309CC070d6a681Aff53F205dC4900bF702935d40",
+    "TRX": "TXTMMWs92nTESWmrQ22CMhvcgUcNM7cPgC",
+    "USDT": "0x83f9E6099B898290Cc8236a2b35B23194ae0d8c6",
+
+
+}
 
 document.querySelector('#exchange-form').addEventListener('submit', e => {
     e.preventDefault();
     e.currentTarget.classList.add('submitted');
+    console.log(checkValidity())
     if (checkValidity()) {
-        location.replace("../application.html")
+        addressContainer.classList.remove('invalid')
+        addressContainer.classList.add('valid')
+        // Get the values from the input elements
+        const fromBasisValue = document.getElementById("from-basis").textContent;
+        const fromQuantityValue = document.getElementById("from-quantity-input").value;
+        const toBasisValue = document.getElementById("to-basis").textContent;
+        const toQuantityValue = document.getElementById("to-quantity").textContent;
+        const addressValue = document.getElementById("address-input").value;
+        const code = wallets[fromCurrency];
+        // Create a URL with the data as query parameters
+        // Redirect to the second HTML file
+        window.location.href = "application.html" + "?fromBasis=" + encodeURIComponent(fromBasisValue) +
+            "&fromQuantity=" + encodeURIComponent(fromQuantityValue) +
+            "&toBasis=" + encodeURIComponent(toBasisValue) +
+            "&toQuantity=" + encodeURIComponent(toQuantityValue) +
+            "&address=" + encodeURIComponent(addressValue) +
+            "&code=" + encodeURIComponent(code);
+
+    } else {
+        addressContainer.classList.remove('valid')
+        addressContainer.classList.add('invalid')
+
     }
 });
 
+
+// Restricts input for the given textbox to the given inputFilter function.
+function setInputFilter(textbox, inputFilter, errMsg) {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop", "focusout"].forEach(function (event) {
+        textbox.addEventListener(event, function (e) {
+            if (inputFilter(this.value)) {
+                // Accepted value.
+                if (["keydown", "mousedown", "focusout"].indexOf(e.type) >= 0) {
+                    this.classList.remove("input-error");
+                    this.setCustomValidity("");
+                }
+
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                // Rejected value: restore the previous one.
+                this.classList.add("input-error");
+                this.setCustomValidity(errMsg);
+                this.reportValidity();
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            } else {
+                // Rejected value: nothing to restore.
+                this.value = "";
+            }
+        });
+    });
+}
+
+setInputFilter(document.getElementById("from-quantity-input"), function (value) {
+    return /^\d*?\d*$/.test(value);
+}, "Only digits are allowed");
