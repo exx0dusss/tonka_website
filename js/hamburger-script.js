31d71668ce96ac4bce0261524d0de8623be83e34
exx0dusss <82311111+exx0dusss@users.noreply.github.com>
2023-08-06 20:53:39 +0200

    ss
    ss

--- js/hamburger-script.js
+++ js/hamburger-script.js
@@ -2,5 +2,8 @@ $(document).ready(function () {
     $('#hamburger-button').click(function () {
         $(this).toggleClass('open');
         $('#header-nav-container').toggleClass('open');
+        document.body.classList.toggle('no-scroll')
+
     });
 });
+
